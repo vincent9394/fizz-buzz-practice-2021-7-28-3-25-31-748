2ae222029d2a85233a97ac085643a7126ed7b3f0
vincent <luk96515@gmail.com>
2021-10-21 13:10:31 +0800

    [FizzBuzz][Vincent] test:create test for given multiple of 3 5 7

--- src/main/java/tdd/fizzbuzz/FizzBuzz.java
+++ src/main/java/tdd/fizzbuzz/FizzBuzz.java
@@ -2,7 +2,9 @@ package tdd.fizzbuzz;
 
 public class FizzBuzz {
     public String countOff(int order) {
-
+        if (order%3 == 0) return "Fizz";
+        if (order%5 == 0) return "Buzz";
+        if (order%7 == 0) return "Whizz";
         return String.valueOf(order);
 
     }
--- src/test/java/tdd/fizzbuzz/FizzBuzzTest.java
+++ src/test/java/tdd/fizzbuzz/FizzBuzzTest.java
@@ -1,13 +1,20 @@
 package tdd.fizzbuzz;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class FizzBuzzTest {
+    private FizzBuzz fizzBuzz;
+    @BeforeEach
+    public void setup() {
+         fizzBuzz = new FizzBuzz();
+    }
+
     @Test
     public void should_return_order_when_count_off_given_order_number() {
-        FizzBuzz fizzBuzz = new FizzBuzz();
+
 
         //given
         int order =1;
@@ -19,5 +26,42 @@ public class FizzBuzzTest {
         //then
         assertEquals(expected,actual);
 
+    }
+    @Test
+    public void should_return_Fizz_when_count_off_given_order_number_multiple_of_3(){
+        //given
+        int order = 3;
+        String expected ="Fizz";
+
+       // when
+        String actual = fizzBuzz.countOff(order);
+
+        //then
+        assertEquals(expected,actual);
+    }
+    @Test
+    public void should_return_Buzz_when_count_off_given_order_number_multiple_of_5(){
+        //given
+        int order = 5;
+        String expected ="Buzz";
+
+        // when
+        String actual = fizzBuzz.countOff(order);
+
+        //then
+        assertEquals(expected,actual);
+    }
+
+    @Test
+    public void should_return_Whizz_when_count_off_given_order_number_multiple_of_7(){
+        //given
+        int order = 7;
+        String expected ="Whizz";
+
+        // when
+        String actual = fizzBuzz.countOff(order);
+
+        //then
+        assertEquals(expected,actual);
     }
 }
